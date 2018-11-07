a815b33071a712d0c0396dd58c1c46a58142f9d7
jake-orielly <jake.orielly@gmail.com>
2018-11-06 23:09:51 -0500

    Using twitter API I gathered 340,000 tweets conatining the word 'vote' over 12 hours on election day. Now to do some data science

---
+++ midtermTwitter/common_words.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+import json
+
+#with open('../../../../../../../Volumes/jakesExternalDrive/tweets.txt','r') as f:
+with open('tweets.txt','r') as f:
+    file = f.read()
+    file = file.split('\n\n')[:-1]
+    count = 0
+    time_counts = Counter(json.loads(item)['created_at'].split(' ')[3][:4] for item in file)
+
+    counter = Counter(word.lower()                      # lowercase words
+                      for item in file
+                      for word in json.loads(item)['text'].strip().split(' ')  # split on spaces
+                      if word)
+
+times = sorted(time_counts)
+time_vals = [time_counts[time] for time in times]
+plt.plot(times, time_vals)
+plt.ylabel('# of tweets')
+plt.title('That\'s a lot of tweets!')
+plt.show()
--- midtermTwitter/twitterAPI.py
+++ midtermTwitter/twitterAPI.py
@@ -1,34 +1,42 @@
 from twython import TwythonStreamer
-import json
+import io, json
 
 # appending data to a global variable is pretty poor form
 # but it does make life easier
 
-tweets = []
 with open('credentials.json', 'r') as f:
     datastore = json.load(f)
 
 class MyStreamer(TwythonStreamer):
+    tweets = 0
     """our own subclass of twythonstreamer that specifies how to interact with the stream"""
-
     def on_success(self,data):
         """what do when twitter sends us data?"""
+        self.tweets+= 1
+        try:
+            if data['lang'] == 'en': #only want english tweets
+                with io.open('../../../../../../../Volumes/jakesExternalDrive/tweets.txt', 'a', encoding='utf-8') as f:
+                    f.write(json.dumps(data, ensure_ascii=False))
+                    f.write('\n'.decode('utf-8'))
+                    f.write('\n'.decode('utf-8'))
+                print("Recieved tweet #"+str(self.tweets))
+        except:
+                print("???")
 
-        if data['lang'] == 'en': #only want english tweets
-            tweets.append(data)
-            print("Recieved tweet #", len(tweets))
 
-        #stop when we have enough
-        if len(tweets) >= 10:
-            self.disconnect()
+#self.disconnect()
 
     def on_error(self, status_code, data):
+        print("ERROR #1")
         print(status_code, data)
         self.disconnect()
 
 stream = MyStreamer(datastore['apiKey'], datastore['apiSecretKey'], datastore['accessToken'], datastore['accessTokenSecret'])
 
 #starts consuming public statuses that contain the keyword 'data'
-stream.statuses.filter(track='vote')
+while True:
+    try:
+        stream.statuses.filter(track='vote')
+    except:
+        print("ERROR #2")
 
-print(tweets[-1]['text'])
