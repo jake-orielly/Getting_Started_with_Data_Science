dc95cad1eee6f4b2f8938846fa856395991ad932
jake-orielly <jake.orielly@gmail.com>
2018-11-09 14:41:39 -0500

    Added simple linear regression

--- machineLearning/functions.py
+++ machineLearning/functions.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import math
+import math, random
 
 
 
@@ -25,5 +25,69 @@ def magnitude(v):
 def distance(v, w):
     return magnitude(vector_subtract(v,w))
 
-def mean(args):
-    return sum(args)/len(args)
+def mean(x):
+    return sum(x) / len(x)
+
+def de_mean(x):
+    x_bar = mean(x)
+    return [x_i - x_bar for x_i in x]
+
+def sum_of_squares(v):
+    return dot(v,v)
+
+def variance(x):
+    n = len(x)
+    deviations = de_mean(x)
+    return sum_of_squares(deviations) / (n-1)
+
+def covariance(x, y):
+    n = len(x)
+    return dot(de_mean(x), de_mean(y)) / (n - 1)
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+def correlation(x,y):
+    stdev_x = standard_deviation(x)
+    stdev_y = standard_deviation(y)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(x,y) / stdev_x / stdev_y
+    else:
+        return 0
+
+def in_random_order(data):
+    """generator that returns the elements of data in random order"""
+    indexes = [i for i, _ in enumerate(data)]   # create a list of indexes
+    random.shuffle(indexes)                     # shuffle them
+    for i in indexes:
+        yield data[i]
+
+def scalar_multiply(c, v):
+    """c is a number, v is a vector"""
+    return [c * v_i for v_i in v]
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    data = zip(x,y)
+    theta = theta_0     #initial guess
+    alpha = alpha_0     #initial step size
+    min_theta, min_value = None, float("inf")   #the min so far
+    iterations_with_no_improvement = 0
+    #if we ever go 100 iterations with no improvement, stop
+    while iterations_with_no_improvement < 100:
+        value = sum(target_fn(x_i, y_i, theta) for x_i, y_i in data)
+        
+        if value < min_value:
+            #if we've found a new min, remember it and go to original step size
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            #otherwise we're not improving, so try shrinking step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+        
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
