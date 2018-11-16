fe029b31cf6c046ff94a97d0082dfcd9e4ef9f4c
jake-orielly <jake.orielly@gmail.com>
2018-11-15 22:58:12 -0500

    Finished naturalLanguageProcessing.py

--- machineLearning/naturalLanguageProcessing.py
+++ machineLearning/naturalLanguageProcessing.py
@@ -45,46 +45,214 @@ for paragraph in content("p"):
     words = re.findall(regex, fix_unicode(paragraph.text))
     document.extend(words)
 
-bigrams = zip(document, document[1:])
-transitions = defaultdict(list)
+def newBigramSentence():
+    bigrams = zip(document, document[1:])
+    transitions = defaultdict(list)
 
-for prev, current in bigrams:
-    transitions[prev].append(current)
+    for prev, current in bigrams:
+        transitions[prev].append(current)
 
-def generate_using_bigrams():
-    current = "."   # next word will be start of a sentence
-    result = []
-    while True:
-        next_word_candidates = transitions[current] #bigrams (current, _)
-        current = random.choice(next_word_candidates)   #choose one at random
-        result.append(current)  #append it to the results
-        if current == "." : return " ".join(result) # if "." we're done
+    def generate_using_bigrams():
+        current = "."   # next word will be start of a sentence
+        result = []
+        while True:
+            next_word_candidates = transitions[current] #bigrams (current, _)
+            current = random.choice(next_word_candidates)   #choose one at random
+            result.append(current)  #append it to the results
+            if current == "." : return " ".join(result) # if "." we're done
 
 #print(generate_using_bigrams())
+def newTrigramSentence():
+    trigrams = zip(document, document[1:], document[2:])
+    trigram_transitions = defaultdict(list)
+    starts = []
 
-trigrams = zip(document, document[1:], document[2:])
-trigram_transitions = defaultdict(list)
-starts = []
+    for prev, current, next in trigrams:
+        if prev == ".":  # if the previous "word" was a period
+            starts.append(current)  # then this is a start word
 
-for prev, current, next in trigrams:
-    if prev == ".":  # if the previous "word" was a period
-        starts.append(current)  # then this is a start word
+        trigram_transitions[(prev, current)].append(next)
 
-    trigram_transitions[(prev, current)].append(next)
+    def generate_using_trigrams():
+        current = random.choice(starts) # choose a random starting word
+        prev = "."  # and precede it with a "."
+        result = [current]
+        while True:
+            next_word_candidates = trigram_transitions[(prev, current)]
+            next_word = random.choice(next_word_candidates)
 
-def generate_using_trigrams():
-    current = random.choice(starts) # choose a random starting word
-    prev = "."  # and precede it with a "."
-    result = [current]
-    while True:
-        next_word_candidates = trigram_transitions[(prev, current)]
-        next_word = random.choice(next_word_candidates)
+            prev, current = current, next_word
+            result.append(current)
 
-        prev, current = current, next_word
-        result.append(current)
+            if current == ".":
+                return " ".join(result)
 
-        if current == ".":
-            return " ".join(result)
+    print(generate_using_trigrams())
 
-print(generate_using_trigrams())
+# --- Sentence Generation Using Grammars ---
+def newGrammarSentence():
+    grammar = {
+        "_S"  : ["_NP _VP"],
+            "_NP" : ["_N",
+                     "_A _NP _P _A _N"],
+                     "_VP" : ["_V",
+                              "_V _NP"],
+                     "_N"  : ["data science", "Python", "regression"],
+                     "_A"  : ["big", "linear", "logistic"],
+                     "_P"  : ["about", "near"],
+                     "_V"  : ["learns", "trains", "tests", "is"]
+    }
+
+    def is_terminal(token):
+        return token[0] != "_"
+
+    def expand(grammer, tokens):
+        for i, token in enumerate(tokens):
+
+            #skip over terminals
+            if is_terminal(token): continue
+
+            #if we get here, we found a non terminal token, so we need to choose a random replacement
+            replacement = random.choice(grammer[token])
+
+            if is_terminal(replacement):
+                tokens[i] = replacement
+            else:
+                tokens = tokens[:i] + replacement.split() + tokens[(i+1):]
+            return expand(grammar, tokens)
+
+        #if we get here, we had all terminals ad are done
+        return tokens
+
+    def generate_sentence(grammar):
+        return expand(grammar, ["_S"])
+
+    print(generate_sentence(grammar))
+
+# --- Gibbs Sampling ---
+def roll_a_die():
+    return random.choice([1,2,3,4,5,6])
+
+def direct_sample():
+    d1 = roll_a_die()
+    d2 = roll_a_die()
+    return d1, d1+d2
+
+def random_y_given_x(x):
+    return x + roll_a_die()
+
+def random_x_given_y(y):
+    if y <= 7:
+        return random.randrange(1,y)
+    else:
+        return random.randrange(y-6,7)
+
+def gibbs_sample(num_iters=100):
+    x,y = 1,2
+    for _ in range(num_iters):
+        x = random_x_given_y(y)
+        y = random_y_given_x(x)
+    return x,y
+
+# ---
+def sample_from(weights):
+    """returns i with probability weights[i] / sum(weights)"""
+    total = sum(weights)
+    rnd = total * random.random()   # uniform between 0 and total
+    for i, w in enumerate(weights):
+        rnd -= w #return the smallest i such that
+        if rnd <= 0: return i   #weights[0] + ... + weights[i] >= rnd
+
+documents = [
+             ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
+             ["NoSQL", "MongoDB", "Cassandra", "HBase", "Postgres"],
+             ["Python", "scikit-learn", "scipy", "numpy", "statsmodels", "pandas"],
+             ["R", "Python", "statistics", "regression", "probability"],
+             ["machine learning", "regression", "decision trees", "libsvm"],
+             ["Python", "R", "Java", "C++", "Haskell", "programming languages"],
+             ["statistics", "probability", "mathematics", "theory"],
+             ["machine learning", "scikit-learn", "Mahout", "neural networks"],
+             ["neural networks", "deep learning", "Big Data", "artificial intelligence"],
+             ["Hadoop", "Java", "MapReduce", "Big Data"],
+             ["statistics", "R", "statsmodels"],
+             ["C++", "deep learning", "artificial intelligence", "probability"],
+             ["pandas", "R", "Python"],
+             ["databases", "HBase", "Postgres", "MySQL", "MongoDB"],
+             ["libsvm", "regression", "support vector machines"]
+             ]
+
+K = 4
+
+# a list of Counters, one for each document
+document_topic_counts = [Counter() for _ in documents]
+
+# a list of Counters, one for each topic
+topic_word_counts = [Counter() for _ in range(K)]
+
+# a list of numbers, one for each topic
+topic_counts = [0 for _ in range(K)]
+
+#a list of numbers, one for each document
+document_lengths = map(len, documents)
+
+distinct_words = set(word for document in documents for word in document)
+W = len(distinct_words)
+D = len(documents)
+
+def p_topic_given_document(topic, d, alpha=0.1):
+    """the fraction of words in document d that are assgined to topic (plus smoothing)"""
+    return ((document_topic_counts[d][topic] + alpha)/(document_lengths[d] + K * alpha))
+
+def p_word_given_topic(word, topic, beta=0.1):
+    """the fratcion of words assigned to topic that equal word (plus smoothing)"""
+    return ((topic_word_counts[topic][word] + beta) / (topic_counts[topic] + W * beta))
+
+def topic_weight(d, word, k):
+    """given a document ad a word in that document, return the weight for the kth topic"""
+    return p_word_given_topic(word, k) * p_topic_given_document(k, d)
+
+def choose_new_topic(d, word):
+    return sample_from([topic_weight(d, word, k)
+                        for k in range(K)])
+
+random.seed(0)
+document_topics = [[random.randrange(K) for word in document]
+                   for document in documents]
+
+for d in range(D):
+    for word, topic in zip(documents[d], document_topics[d]):
+        document_topic_counts[d][topic] += 1
+        topic_word_counts[topic][word] += 1
+        topic_counts[topic] += 1
+
+for iter in range(1000):
+    for d in range(D):
+        for i, (word, topic) in enumerate(zip(documents[d],
+                                              document_topics[d])):
+            # remove this word / topic from the counts, so that it doesn't influence the weights
+            document_topic_counts[d][topic] -= 1
+            topic_word_counts[topic][word] -= 1
+            topic_counts[topic] -= 1
+            document_lengths[d] -= 1
+            
+            # choose a new topic based on the weights
+            new_topic = choose_new_topic(d, word)
+            document_topics[d][i] = new_topic
+
+            # and now add it back to the counts
+            document_topic_counts[d][new_topic] += 1
+            topic_word_counts[new_topic][word] += 1
+            topic_counts[new_topic] += 1
+            document_lengths[d] += 1
+
+topic_names = ["Big Data and programming languages",
+               "Python and statistics",
+               "databases",
+               "machine learning"]
+
+for document, topic_counts in zip(documents, document_topic_counts):
+    print(document)
+    for topic, count in topic_counts.most_common():
+        if count > 0:
+            print (topic_names[topic], count,)
 
