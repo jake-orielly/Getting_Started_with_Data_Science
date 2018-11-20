eca3e1e85eb3c440e09d2186ed48d912cd6905ed
jake-orielly <jake.orielly@gmail.com>
2018-11-20 17:09:49 -0500

    Finished network analysis

--- networkAnalysis.py
+++ networkAnalysis.py
@@ -120,3 +120,63 @@ def matrix_operate(A, v):
     v_as_matrix = vector_as_matrix(v)
     product = matrix_multiply(A, v_as_matrix)
     return vector_from_matrix(product)
+
+def find_eigenvector(A, tolerance=0.00001):
+    guess = [random.random() for __ in A]
+
+    while True:
+        result = matrix_operate(A, guess)
+        length = magnitude(result)
+        next_guess = scalar_multiply(1/length, result)
+
+        if distance(guess, next_guess) < tolerance:
+            return next_guess, length #eigenvector, eigenvalue
+        guess = next_guess
+
+def entry_fn(i, j):
+    return 1 if (i, j) in friendships or (j, i) in friendships else 0
+
+n = len(users)
+adjacency_matrix = make_matrix(n, n, entry_fn)
+eigenvector_centralities, _ = find_eigenvector(adjacency_matrix)
+
+# --- directed graphs ---
+
+endorsements = [(0, 1), (1, 0), (0, 2), (2, 0), (1, 2), (2, 1), (1, 3),
+                (2, 3), (3, 4), (5, 4), (5, 6), (7, 5), (6, 8), (8, 7), (8, 9)]
+
+for user in users:
+    user["endorses"] = [] # add one list to track outgoing endorsements
+    user["endorsed_by"] = [] # add another to track endorsements
+
+for source_id, target_id in endorsements:
+    users[source_id]["endorses"].append(users[target_id])
+    users[target_id]["endorsed_by"].append(users[source_id])
+
+endorsements_by_id = [(user["id"], len(user["endorsed_by"]))
+                      for user in users]
+
+sorted(endorsements_by_id,
+       key=lambda(user_id, num_endorsements): num_endorsements,
+       reverse=True)
+
+def page_rank(users, damping = 0.85, num_iters = 100):
+    #initially distribute PageRank evenly
+    num_users = len(users)
+    pr = {user["id"] : 1 / num_users for user in users}
+
+    #this is the small fraction of PageRank that each node gets each iteration
+    base_pr = (1- damping)/num_users
+
+    for __ in range(num_iters):
+        next_pr = {user["id"] : base_pr for user in users}
+        for user in users:
+            #distribute PageRank to outgoing links
+            links_pr = pr[user["id"]] * damping
+            for endorsee in user["endorses"]:
+                next_pr[endorsee["id"]] += links_pr / len(user["endorses"])
+        pr = next_pr
+    return pr
+
+for user_id, pr in page_rank(users).iteritems():
+    print(user_id, pr)
